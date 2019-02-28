574cd532f53358fe8b246222726b59d2c6498bf2
Carlos Clares Cucala <carlos.clares.cucala@everis.com>
2019-02-28 19:41:30 +0100

    create Slide class representation

---
+++ Hashcode.IO/Slide.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Hashcode.IO
+{
+    public abstract class Slide
+    {
+        public abstract string[] Tags();
+    }
+
+    public class VerticalSlide : Slide
+    {
+        public Photo PhotoA { get; set; }
+
+        public Photo PhotoB { get; set; }
+
+        public override string[] Tags()
+        {
+            return PhotoA.Tags.Union(this.PhotoB.Tags).ToArray();
+        }
+    }
+
+    public class HorizontalSlide : Slide
+    {
+        public Photo PhotoA { get; set; }
+
+        public override string[] Tags()
+        {
+            return this.PhotoA.Tags;
+        }
+    }
+}
