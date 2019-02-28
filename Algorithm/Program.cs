7bb5edba7b912c01e05ca6691df8533bb3460e8d
Carlos Clares Cucala <carlos.clares.cucala@everis.com>
2019-02-28 20:25:16 +0100

    use async main

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -13,9 +13,9 @@ namespace Algorithm
         {
             var watch = new Stopwatch();
             watch.Start();
-            var photos = await Reader.Read(File.OpenRead("a_example.txt"));
+            var photos = await Reader.Read(File.OpenRead("e_shiny_selfies.txt"));
             var slides = Calculator.Calculate(photos);
-            await Writer.Write(slides, File.OpenWrite("a_example_output.txt"));
+            await Writer.Write(slides, File.OpenWrite("e_shiny_selfies_output.txt"));
             watch.Stop();
             Console.WriteLine($"Elapsed: {watch.ElapsedMilliseconds/1000.0}");
             Console.ReadLine();
