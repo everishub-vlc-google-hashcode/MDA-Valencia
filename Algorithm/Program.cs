ef56575235db46388e8888f117289427271bff31
Sergio Moreno <sergio.moreno.calzada@everis.com>
2019-02-28 17:43:11 +0100

    solution creation

---
+++ Algorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await Task.FromResult(0);
+        }
+    }
+}
